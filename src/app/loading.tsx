82d0ea5d63831115ddee38642a5d7d127d7edada
Minna Jarsma <minna.jarsma@gmail.com>
2024-01-03 11:08:53 +0200

    Use CircularProgress in home page loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,27 +1,10 @@
-import Image from "next/image";
 import Grid from "@mui/material/Grid";
-import {
-  THUMBNAIL_HEIGHT,
-  THUMBNAIL_WIDTH,
-  shimmer,
-  toBase64,
-} from "@/utils/image";
+import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Loading() {
   return (
-    <Grid container spacing={2} alignItems="center" justifyContent="center">
-      {Array.from(new Array(20)).map((_, index) => (
-        <Grid key={index} item xs={12} md={3}>
-          <Image
-            src={`data:image/svg+xml;base64,${toBase64(
-              shimmer(THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT),
-            )}`}
-            width={THUMBNAIL_WIDTH}
-            height={THUMBNAIL_HEIGHT}
-            alt=""
-          />
-        </Grid>
-      ))}
+    <Grid container alignItems="center" justifyContent="center">
+      <CircularProgress color="secondary" sx={{ my: 5 }} />
     </Grid>
   );
 }
