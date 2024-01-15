57ff0d83714548ffcdd5d8f1fab2933c12671713
Minna Jarsma <minna.jarsma@gmail.com>
2024-01-15 17:50:23 +0200

    Move use client from ThemeRegistry to theme

--- src/components/ThemeRegistry.tsx
+++ src/components/ThemeRegistry.tsx
@@ -1,15 +1,14 @@
-"use client";
 import * as React from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme/theme";
 
-export default function ThemeRegistry({
-  children,
-}: {
+type ThemeRegistryProps = {
   children: React.ReactNode;
-}) {
+};
+
+export default function ThemeRegistry({ children }: ThemeRegistryProps) {
   return (
     <AppRouterCacheProvider>
       <ThemeProvider theme={theme}>
